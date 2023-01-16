688a3378c3268ce3c5bbeb9f24e4528c42ad41e7
Dean <dean.kinane@gmail.com>
2023-01-16 09:25:23 +0000

    Added commands and queries for events and orders. Fixed typo in persistence namespace. Added FluentValidation and validators. Added service registration extension method. Added custom exceptions.

--- src/Core/GloboTicket.TicketManagement.Application/Contracts/Persistance/ICategoryRepository.cs
+++
@@ -1,9 +0,0 @@
-using GloboTicket.TicketManagement.Domain.Entities;
-
-namespace GloboTicket.TicketManagement.Application.Contracts.Persistance
-{
-    public interface ICategoryRepository : IAsyncRepository<Category>
-    {
-        
-    }
-}
